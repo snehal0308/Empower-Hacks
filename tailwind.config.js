8a63dc96b9ce29a27aa44237b33b5a748bfc6e1f
kihea <kihea@icloud.com>
2023-08-19 05:26:02 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["public/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+            'lexend': ["Lexend"]
+        },
+        colors: {
+            'main': '#fff',
+        },
+      },
+    },
+    plugins: [],
+  }
